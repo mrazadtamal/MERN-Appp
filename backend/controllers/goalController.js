a5674e48e4ec6986f82e8a420328ba8a45d6d59a
mrazadtamal <mrazadt@gmail.com>
2022-02-15 18:46:46 +0600

    async

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,35 @@
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoal = (req, res) => {
+const getGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ name: `get goal` });
-};
+});
 
 // @desc    set goals
 // @route   post /api/goals
 // @access  Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
 
     throw new Error("please enter text");
   }
   res.status(200).json({ name: `set goal` });
-};
+});
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ name: `update goals ${req.params.id}` });
-};
+});
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ name: `delete goals ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoal,
