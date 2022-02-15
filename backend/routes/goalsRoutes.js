e91172c5f5d1c98894b6d5d6344effee2bd6fbb8
mrazadtamal <mrazadt@gmail.com>
2022-02-15 12:56:30 +0600

    routes

--- backend/routes/goalsRoutes.js
+++ backend/routes/goalsRoutes.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("MERN");
+});
+router.post("/", (req, res) => {
+  res.status(200).json({ name: "azad" });
+});
+router.get("/", (req, res) => {
+  res.send("MERN");
+});
+router.get("/", (req, res) => {
+  res.send("MERN");
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("MERN");
-});
+app.use("/api/goals", require("./routes/goalsRoutes.js"));
 
 app.listen(port, () => {
   console.log(`app server on at ${port}`);
