03611078c35ecf4f96b61b67de88e01295c6cfd7
mrazadtamal <mrazadt@gmail.com>
2022-02-15 12:43:14 +0600

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("azad");
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("MERN");
+});
+
+app.listen(port, () => {
+  console.log(`app server on at ${port}`);
+});
